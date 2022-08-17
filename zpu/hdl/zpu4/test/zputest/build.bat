916e5d64022312ad8e8371f281fdcda5d21b0fd2
FREEWING-JP <freewing.jp@gmail.com>
2022-08-17 22:55:49 +0900

    add zputest.c

---
+++ zpu/hdl/zpu4/test/zputest/build.bat
@@ -0,0 +1,7 @@
+PATH=..\..\..\..\..\..\install_exe\bin;%PATH%
+
+zpu-elf-gcc -O3 -phi .\zputest.c -o zputest.elf -Wl,--relax -Wl,--gc-sections  -g
+zpu-elf-objdump --disassemble-all >zputest.dis zputest.elf
+zpu-elf-objcopy -O binary zputest.elf zputest.bin
+java -classpath ..\..\..\..\sw\simulator\zpusim.jar com.zylin.zpu.simulator.tools.MakeRam zputest.bin >zputest.ram
+
